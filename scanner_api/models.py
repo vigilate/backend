fe82b0243c3fa29881c621fda315354ce06e0d42
Vigilate <vigilate_2017@labeip.epitech.eu>
2016-02-08 18:08:20 +0000

    add auto increment in user id field

--- scanner_api/models.py
+++ scanner_api/models.py
@@ -14,7 +14,7 @@ class Vuln(models.Model):
     score = models.IntegerField()
 
 class User(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True)
+    id = models.AutoField(primary_key=True, unique=True)
     username = models.CharField(max_length=20, unique=True)
     email = models.CharField(max_length=20, unique=True)
     password = models.TextField(null=False)
