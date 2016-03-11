cb4b12f68cc585f16f2b3270117e52b9ceb07578
Vigilate <vigilate_2017@labeip.epitech.eu>
2016-03-11 22:49:40 +0000

    query parsing modified for the tests

--- scanner_api/utils.py
+++ scanner_api/utils.py
@@ -1,8 +1,8 @@
 import json
 
 def get_query(request):
-    if request.method == "POST" and "query" in request.data:
-        query = request.data['query']
+    if request.method == "POST":
+        query = list(request.data)[0]
         if query:
             try:
                 query = json.loads(query)
