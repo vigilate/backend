da620b86907d0ca371e56c6efd88551e8cb2ec2c
eax64 <inceax64@gmail.com>
2016-03-21 17:56:33 +0000

    use env to specify python version in install-pyargon2.sh

--- install-pyargon2.sh
+++ install-pyargon2.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 set -ex
+
+PYTHON="python"
+if [ $(python -c "import sys;print(sys.version.split('.', 1)[0])") -eq "2" ]
+then
+    PYTHON="python3"
+fi
+    
+
 git clone https://github.com/vigilate/module_com_bdd.git
-make -C module_com_bdd
-sudo make -C module_com_bdd install
+PYTHON=$PYTHON make -C module_com_bdd
+sudo PYTHON=$PYTHON make -C module_com_bdd install
