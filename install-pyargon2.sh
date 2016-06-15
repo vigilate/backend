d49c58eaffa3825125c7d793d4a33c4ce6402c0e
eax64 <inceax64@gmail.com>
2016-06-15 14:41:36 +0100

    Fix travis sudo python bug

--- install-pyargon2.sh
+++ install-pyargon2.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
 set -ex
-
-PYTHON="python"
-if [ $(python -c "import sys;print(sys.version.split('.', 1)[0])") -eq "2" ]
-then
-    PYTHON="python3"
-fi
     
 
 git clone https://github.com/vigilate/module_com_bdd.git
-PYTHON=$PYTHON make -C module_com_bdd
-sudo PYTHON=$PYTHON make -C module_com_bdd install
+make -C module_com_bdd
+sudo env PATH=$PATH make -C module_com_bdd install
