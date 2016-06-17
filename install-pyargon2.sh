e4850584db817c17e3ebbee651e85ffa6234d061
eax64 <inceax64@gmail.com>
2016-06-17 20:50:05 +0100

    Change install file to remove the need for travis sudo

--- install-pyargon2.sh
+++ install-pyargon2.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+SUDO=1
+
+if [ $1 = "nosudo" ]
+then
+    SUDO=0
+fi
+
 set -ex
     
 
 git clone https://github.com/vigilate/module_com_bdd.git
 make -C module_com_bdd
-sudo env PATH=$PATH make -C module_com_bdd install
+
+if [ $SUDO -eq 1 ]
+then
+    sudo env PATH=$PATH make -C module_com_bdd install
+else
+    make -C module_com_bdd install
+fi
+
+
