d8375265dca48331f3ca2482e244e2b3937c785c
ampotos <ampotos@gmail.com>
2016-07-06 17:48:53 +0100

    Check vuln on cve add/update and cutsom cve ("name:version") fix #7
    To make the cehck possible we use our custom simplify form of cpe :
    "prog_name:version".

--- vigilate_backend/alerts.py
+++ vigilate_backend/alerts.py
@@ -3,6 +3,7 @@ from vulnerability_manager import models as models_vuln
 from django.db.models import Q
 
 def create_alert(prog, cve, user):
+    #TODO check level of vuln vs level min d'alert in prog
     new_alert = models.Alert(user=user, program=prog, cve=cve)
     new_alert.save()
     #TODO take action to send alert
@@ -13,3 +14,10 @@ def check_prog(prog, user):
 
     for cve in cves:
         create_alert(prog, cve, user)
+
+def check_cve(cve):
+    cpes = cve.cpe.all()
+    progs = models.UserPrograms.objects.filter(cpe__in=cpes)
+
+    for prog in progs:
+        create_alert(prog, cve, prog.user_id)
