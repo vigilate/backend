9914ada310028b9c8075b62d512fc7c242a2650a
fr0gRIP <fr0g.security@gmail.com>
2016-07-10 16:41:04 +0100

    Fix #9 tests are now using external data

--- vigilate_backend/tests.py
+++ vigilate_backend/tests.py
@@ -1,54 +1,57 @@
 import sys
 import json
 import base64
+import random
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase, APIClient
 from vigilate_backend import models
+
+from vigilate_backend.tests_data import *
 
 class UserProgramsTestCase(APITestCase):
     def setUp(self):
         self.client = APIClient()
 
-        credentials = base64.b64encode(b"test:test")
+        credentials = base64.b64encode(b"%b:%b" % (str.encode(userdata['username']), str.encode(userdata['password'])))
 
         self.client.defaults['HTTP_AUTHORIZATION'] = 'Basic ' + str(credentials.decode("utf-8"))
-        User.objects.create_user(username="test", password="test")
+        User.objects.create_user(username=userdata['username'], password=userdata['password'])
 
         self.new_client = models.User()
-        self.new_client.username = "test"
-        self.new_client.password = "UCGeyUPEIw$711cbb3de43417805310ead8e3e4dca6ab5a5f669819a7a3373d9bebdc5986fedfaf8a19cc80f823794d4e14c98d75bad46e55e08aca651f3a2123c0546ad07270f22cb2e27f"
-        self.new_client.email = "test@test.test"
-        self.new_client.user_type = 1
-        self.new_client.contrat = 1
-        self.new_client.id_dealer = 1
+        self.new_client.username = userdata['username']
+        self.new_client.password = userdata['hash']
+        self.new_client.email = userdata['email']
+        self.new_client.user_type = userdata['type']
+        self.new_client.contrat = userdata['contrat']
+        self.new_client.id_dealer = userdata['dealer']
         self.new_client.save()
 
     def test_api_access(self):
-        self.client.login(username="test", password="test")
-        resp = self.client.get("/api/")
+        self.client.login(username=userdata['username'], password=userdata['password'])
+        resp = self.client.get(api_routes['api'])
         self.assertTrue(resp.status_code == 200)
 
     def test_route_alerts(self):
-        self.client.login(username="test", password="test")
-        resp_alerts = self.client.get("/api/alerts/")
+        self.client.login(username=userdata['username'], password=userdata['password'])
+        resp_alerts = self.client.get(api_routes['alerts'])
         self.assertTrue(resp_alerts.status_code == 200)
 
     def test_route_user_progs(self):
-        self.client.login(username="test", password="test")
-        resp_progs = self.client.get("/api/uprog/")
+        self.client.login(username=userdata['username'], password=userdata['password'])
+        resp_progs = self.client.get(api_routes['programs'])
         self.assertTrue(resp_progs.status_code == 200)
 
     def test_route_users(self):
-        self.client.login(username="test", password="test")
-        resp_users = self.client.get("/api/users/")
+        self.client.login(username=userdata['username'], password=userdata['password'])
+        resp_users = self.client.get(api_routes['users'])
         self.assertTrue(resp_users.status_code == 200)
 
     def test_submit_one_program(self):
-        self.client.login(username="test", password="test")
+        self.client.login(username=userdata['username'], password=userdata['password'])
 
-        prog = {"program_name" : "Google Chrome", "program_version" : "51.0", "minimum_score": 0, "poste": 1}
+        prog = random.choice(prog_to_submit)
 
-        resp = self.client.post("/api/uprog/", json.dumps(prog),
+        resp = self.client.post(api_routes['programs'], json.dumps(prog),
                                 content_type="application/x-www-form-urlencoded")
 
         self.assertTrue(resp.status_code == 200)
@@ -60,10 +63,10 @@ class UserProgramsTestCase(APITestCase):
         self.assertTrue(prog_sent == prog_saved)
         
     def test_submit_one_program_json_encoded(self):
-        self.client.login(username="test", password="test")
-        prog = {"program_name" : "Google Chrome", "program_version" : "51.0", "minimum_score": 0,"poste": 1}
+        self.client.login(username=userdata['username'], password=userdata['password'])
+        prog = random.choice(prog_to_submit)
 
-        resp = self.client.post("/api/uprog/", json.dumps(prog),
+        resp = self.client.post(api_routes['programs'], json.dumps(prog),
                                 content_type="application/json")
 
         self.assertTrue(resp.status_code == 200)
@@ -74,18 +77,12 @@ class UserProgramsTestCase(APITestCase):
 
         self.assertTrue(prog_sent == prog_saved)
 
-
     def test_submit_multiples_programs(self):
-        self.client.login(username="test", password="test")
+        self.client.login(username=userdata['username'], password=userdata['password'])
 
-        prog_list = {"programs_list" :
-                     [
-                         {"program_name" : "mozilla firefox", "program_version" : "31.0"},
-                         {"program_name" : "blabla", "program_version" : "2.0.1"}
-                     ],
-                     "poste" : 1}
+        prog_list = random.choice(prog_list_to_submit)
 
-        resp = self.client.post("/api/uprog/", json.dumps(prog_list),
+        resp = self.client.post(api_routes['programs'], json.dumps(prog_list),
                                 content_type="application/x-www-form-urlencoded")
         self.assertTrue(resp.status_code == 200)
         user_progs = models.UserPrograms.objects.filter(user_id=self.new_client.id)
@@ -93,7 +90,6 @@ class UserProgramsTestCase(APITestCase):
         for prog in user_progs:
             elem = {"program_name" : prog.program_name, "program_version" : prog.program_version}
             database_programs_json.append(elem)
-
         
         for sent in prog_list['programs_list']:
             self.assertTrue(sent in database_programs_json)
