3534ac52ac566fa8b7284499974398869b63dfd2
fr0gRIP <fr0g.security@gmail.com>
2016-07-21 01:54:51 +0100

    Fix #68 poste id is now a foreign key
    Need repairs: new Station now added if not specified
    
    Debug line removed

--- vigilate_backend/views.py
+++ vigilate_backend/views.py
@@ -80,6 +80,14 @@ class UserProgramsViewSet(viewsets.ModelViewSet):
         if not query:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+        user = User.objects.filter(id=request.user.id)[0]
+
+        if 'poste' not in query or len(Station.objects.filter(id=int(query['poste']))) == 0:
+            station = Station(user=user, name="default")
+            station.save()
+        else:
+            station = Station.objects.filter(id=query['poste'])[0]
+
         only_one_program = False
         if not "programs_list" in query:
             if not all(x in query for x in ['program_version', 'program_name', 'minimum_score']):
@@ -102,8 +110,9 @@ class UserProgramsViewSet(viewsets.ModelViewSet):
         up_to_date = False
         if TESTING:
             up_to_date = True
+
         for elem in query['programs_list']:
-            prog = UserPrograms.objects.filter(user_id=request.user.id, program_name=elem['program_name'], poste=query['station'])
+            prog = UserPrograms.objects.filter(user_id=request.user.id, program_name=elem['program_name'], poste=station)
 
             # if prog , user is already monitoring the given program, update is needed
             if prog:
@@ -126,7 +135,7 @@ class UserProgramsViewSet(viewsets.ModelViewSet):
 
                 (cpe, up_to_date) =  cpe_updater.get_cpe_from_name_version(elem['program_name'], elem['program_version'], up_to_date)
 
-                new_prog = UserPrograms(user_id=request.user, minimum_score=1, poste=query['station'],
+                new_prog = UserPrograms(user_id=request.user, minimum_score=1, poste=station,
                                         program_name=elem['program_name'], program_version=elem['program_version'], cpe=cpe)
                 if 'minimum_score' in elem:
                     new_prog.minimum_score = int(elem['minimum_score'])
@@ -135,7 +144,7 @@ class UserProgramsViewSet(viewsets.ModelViewSet):
                 alerts.check_prog(new_prog, request.user)
 
             if only_one_program:
-                obj = UserPrograms.objects.get(user_id=request.user.id, program_name=elem['program_name'], poste=query['station'])
+                obj = UserPrograms.objects.get(user_id=request.user.id, program_name=elem['program_name'], poste=station)
                 serializer = self.get_serializer(obj)
                 return Response(serializer.data, status=status.HTTP_200_OK)
 
