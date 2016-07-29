d359597fd00e75e518f739d4081b4c39d62963b0
fr0gRIP <fr0g.security@gmail.com>
2016-07-29 01:44:35 +0100

    Useless data from copied file removed

--- vigilate_backend/tests/test_ScannerDownload_data.py
+++ vigilate_backend/tests/test_ScannerDownload_data.py
@@ -1,7 +1,1 @@
-prog_list_to_submit = {"programs_list" :
-                       [
-                           {"program_name" : "mozilla firefox", "program_version" : "31.0"},
-                           {"program_name" : "blabla", "program_version" : "2.0.1"}
-                       ],
-                       "poste" : "changed"}
 scanner = {"id": 1}
